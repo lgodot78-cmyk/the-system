3de32c1a8c800d6be7b8b9355b89e254f59b9cfe
lgodot78-cmyk <lgodot78@gmail.com>
2026-08-14 12:22:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,1 @@
-
-const CACHE = "the-system-v3";
-const ASSETS = ["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-512.png"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
-});
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(resp => {
-    const copy = resp.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    return resp;
-  }).catch(()=>caches.match("./index.html"))));
-});
+const CACHE='the-system-v3';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
